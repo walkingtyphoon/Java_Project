cf84fe65d6a94b136d0e0fdbc08bbe0d66a38d5d
typhoon <walking_typhoon@163.com>
2020-03-10 16:36:18 +0800

    2020-3-10-16:04

---
+++ src/alibaba/practice/basic/Demo05/Basic51.java
@@ -0,0 +1,14 @@
+package alibaba.practice.basic.Demo05;
+
+/**
+ * @program: Java workspace
+ * @description: 练习Java基础
+ * @author: Typhoon
+ * @create: 2020-02-14 10:44
+ * 谁终将声震人间，必长久深自缄默；谁终将点燃闪电，必长久如云漂泊。
+ **/
+public class Basic51 {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(10));
+    }
+}
---
+++ src/alibaba/practice/basic/Demo05/Basic52.java
@@ -0,0 +1,11 @@
+package alibaba.practice.basic.Demo05;
+
+/**
+ * @program: Java workspace
+ * @description: 练习文件上传
+ * @author: Typhoon
+ * @create: 2020-03-10 16:03
+ * 谁终将声震人间，必长久深自缄默；谁终将点燃闪电，必长久如云漂泊。
+ **/
+public class Basic52 {
+}
